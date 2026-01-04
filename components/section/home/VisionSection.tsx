05814ed9c07fce45f8b42f14b564e45dc2146e5e
Vinzzztty <kevinarnandes21@gmail.com>
2026-01-04 10:26:16 +0700

    fix: bug on vision section

--- components/section/home/VisionSection.tsx
+++ components/section/home/VisionSection.tsx
@@ -55,7 +55,7 @@ export default function VisionSection() {
   ];
 
   return (
-    <section className="py-14 md:py-20 bg-slate-50">
+    <section className="py-14 md:py-20 bg-white">
       <div className="max-w-7xl mx-auto px-6">
         {/* HEADER */}
         <div className="text-center md:text-left mb-12">
